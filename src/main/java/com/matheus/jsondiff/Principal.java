9d3c6047a1b0b52eb6dfcd68ed7121205f9befd8
matheusrobertolima <matheusrio10@hotmail.com>
2023-08-24 15:49:59 -0300

    Versão inicial do diff com as informações de name e type no output do algoritmo.

--- src/main/java/com/matheus/jsondiff/Principal.java
+++ src/main/java/com/matheus/jsondiff/Principal.java
@@ -8,11 +8,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flipkart.zjsonpatch.JsonDiff;
 import java.nio.file.Files;  
 import java.nio.file.Paths;  
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class Principal {
 
@@ -23,14 +26,14 @@ public class Principal {
             String fileName2, file2, location2;
             String location3;
 
-            fileName1 = "calculadora1.json";  
+            fileName1 = "calculadora2.json";  
             location1 = "C:\\Users\\Matheus Lima\\Desktop\\tcc\\json file";   
 
             //Converte o arquivo json em String e salva na variável
             file1 = convertFileIntoString(location1+"\\"+fileName1);  
 
             
-            fileName2 = "calculadora2.json";  
+            fileName2 = "calculadora3.json";  
             location2 = "C:\\Users\\Matheus Lima\\Desktop\\tcc\\json file";  
             
              //Converte o arquivo json em String e salva na variável
@@ -43,31 +46,106 @@ public class Principal {
             JsonNode node2 = mapper.readTree(file2);
             
             //Comparação usando o import zjsonpatch.JsonDiff retornando TODAS as diferenças
-            String diff = JsonDiff.asJson(node1, node2).toString();
-            
-            //Formatação para melhor visualização das diferenças
-            Gson gson = new GsonBuilder().setPrettyPrinting().create();
-            Object[] jsonArray = gson.fromJson(diff, Object[].class);
-            String formattedJson = gson.toJson(jsonArray);
-            System.out.println(formattedJson);
+            String diff = JsonDiff.asJson(node1, node2).toString();        
+              
+            try {
+                
+            //List<String> pathsList = new ArrayList<>();
+            JSONArray jsonArray2 = new JSONArray(diff);
+
+                for (Object obj : jsonArray2) {
+                    JSONObject jsonObject = (JSONObject) obj;
+                    String op = jsonObject.get("op").toString();  
+                    String path = jsonObject.get("path").toString();      
+                    String value = jsonObject.get("value").toString();  
+                    //pathsList.add(path);
+                    System.out.println("op: " + op); 
+                    System.out.println("path: " + path); 
+                    System.out.println("value: " + value); 
+                    String[] pathDividido = path.split("/");                      
+                    
+                    JSONObject jsonComparador = new JSONObject(file2);  
+                    
+                    Map<String, Integer> contagem = contarOcorrencias(pathDividido);
+
+                    String alvo = "$Components";
+                    int ocorrencias = contagem.getOrDefault(alvo, 0);                              
+                    
+                    for(int i=1; i < pathDividido.length ; i++){
+                      
+                    try {
+                         
+                        
+                        if(pathDividido[i].equals("Properties") && pathDividido.length == 3){
+                            JSONObject properties = jsonComparador.getJSONObject("Properties");
+                            String nameValue = properties.getString("$Name");
+                            String typeValue = properties.getString("$Type");
+                            System.out.println("name: " + nameValue);   
+                            System.out.println("type: " + typeValue);
+                        }
+                        else if(pathDividido[i].equals("$Components") && ocorrencias == 1){
+                                                    
+                            JSONObject properties = jsonComparador.getJSONObject(pathDividido[i - 1]);
+                            
+                            
+                            JSONArray componentsArray = properties.getJSONArray(pathDividido[i]);
+                            JSONObject secondComponent = componentsArray.getJSONObject(Integer.parseInt(pathDividido[i + 1]));
+                            String nameValue = secondComponent.getString("$Name");
+                            String typeValue = secondComponent.getString("$Type");
+                            System.out.println("name: " + nameValue);  
+                            System.out.println("type: " + typeValue);
+                        }
+                        else if (pathDividido[i].equals("$Components") && ocorrencias > 1 && i == 4) {
+                            
+                            JSONObject properties = jsonComparador.getJSONObject(pathDividido[1]);
+                            
+                            
+                            JSONArray componentsArray = properties.getJSONArray(pathDividido[2]);
+                            JSONObject secondComponent = componentsArray.getJSONObject(Integer.parseInt(pathDividido[3]));
+                            JSONArray componentsArray2 = secondComponent.getJSONArray(pathDividido[4]);
+                            JSONObject secondComponent2 = componentsArray2.getJSONObject(Integer.parseInt(pathDividido[5]));
+                            String nameValue = secondComponent2.getString("$Name");
+                            String typeValue = secondComponent2.getString("$Type");
+                            System.out.println("name: " + nameValue);  
+                            System.out.println("type: " + typeValue);
+                            
+                        }
+                        }
+                        catch (ArrayIndexOutOfBoundsException e) {
+                        // Tratamento da exceção ArrayIndexOutOfBoundsException
+                            String nameValue = "Components";
+                            String typeValue = "Components";
+                            System.out.println("name: " + nameValue);  
+                            System.out.println("type: " + typeValue);
+              
+                        }
+                    } 
+                    
+                   System.out.println("--------");
+                    
+                }  
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             
             //Localização de salvamento do diff em formato json
             location3 = "C:\\Users\\Matheus Lima\\Desktop\\tcc\\json file\\diff.json";
             
             //Método utilizando as bibliotecas BufferedWriter, FileWriter e IOException para criar o arquivo json no caminho acima
-            escreverJsonNoArquivo(formattedJson, location3);
+            //escreverJsonNoArquivo(formattedJson, location3);
             
             }
       
         //Método para fazer a leitura do arquivo e transforma-lo em string
         public static String convertFileIntoString(String file)throws Exception  
-            {  
+        {  
 
             String result;  
             result = new String(Files.readAllBytes(Paths.get(file)));  
 
             return result;  
-            } 
+        } 
     
         private static void escreverJsonNoArquivo(String jsonString, String caminhoArquivo) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
@@ -76,6 +154,26 @@ public class Principal {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+        }
+        
+        public static boolean isNumeric(String str) {
+        try {
+            int d = Integer.parseInt(str);
+        } catch (NumberFormatException | NullPointerException e) {
+            return false;
+        }
+        return true;
+        }
+        
+        public static Map<String, Integer> contarOcorrencias(String[] lista) {
+        Map<String, Integer> contagem = new HashMap<>();
+        
+        for (String str : lista) {
+            contagem.put(str, contagem.getOrDefault(str, 0) + 1);
+        }
+        
+        return contagem;
+        }
        
-}
+        
+} 
